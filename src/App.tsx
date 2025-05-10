312df5acd1c16c2a7b5f340f4697271fbe5d1f97
user0k <glucus2@yandex.ru>
2025-05-10 15:15:52 +0300

    feat: add mock data to Card

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
 import Card from "./components/Card";
 
+const mockedCardArgs = {
+  userName: "Jack Smith",
+  details: {
+    companyName: "Webflow",
+    companyIconUrl: "/webflow.svg",
+    jobTitle: "Senior Product Designer",
+    nickName: "@kingjack",
+    pronouns: "He/Him",
+  },
+  location: {
+    city: "Vancouver",
+    country: "Canada",
+  },
+  image: {
+    url: "/image.jpg",
+  },
+};
+
 function App() {
   return (
     <div className="h-full min-h-screen flex items-center text-gray-900 p-6 bg-gradient-to-br from-gray-100 to-gray-300">
-      <Card />
+      <Card {...mockedCardArgs} />
     </div>
   );
 }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,21 @@
 import { getFlagEmoji } from "../utils/getFlagEmoji";
 
-function Card() {
+interface CardProps {
+  userName: string;
+  details: {
+    companyName: string;
+    jobTitle: string;
+    companyIconUrl?: string;
+    nickName?: string;
+    pronouns?: string;
+  };
+  location: { country: string; city: string };
+  image: {
+    url: string;
+  };
+}
+
+function Card({ userName, details, location, image: { url } }: CardProps) {
   const flagEmoji = getFlagEmoji("Canada");
 
   return (
@@ -10,7 +25,7 @@ function Card() {
           <div className="relative w-40 h-40 overflow-hidden rounded-full border-6 border-white max-sm:w-24 max-sm:h-24 max-sm:border-3">
             <img
               className="absolute top-[-20px] h-50 w-auto object-cover max-sm:top-[-12px] max-sm:h-30"
-              src={"/image.jpg"}
+              src={url}
               alt="avatar"
             />
           </div>
@@ -27,33 +42,47 @@ function Card() {
           </button>
         </div>
         <h1 className="font-semibold sm:text-3xl text-2xl text-gray-800 mb-6">
-          Jack Smith
+          {userName}
         </h1>
         <ul className="flex flex-wrap items-center gap-2 text-xl pb-2 max-sm:flex-col max-sm:items-start">
-          <li>kingjack</li>
+          {details.nickName && <li>{details.nickName}</li>}
           <li>
             <p className="flex items-center gap-2">
               <span className="font-black text-gray-500 pb-2 max-sm:hidden">
                 .
               </span>
-              Senior Product Designer
+              {details.jobTitle}
             </p>
           </li>
           <li className="flex items-center gap-2">
             <span className="text-gray-500">at</span>
-            <img src="/webflow.svg" alt="Webflow logo" className="max-h-6" />
-            Webflow
-            <p className="flex items-center gap-2 text-gray-500">
-              <span className="font-black text-gray-500 pb-2">.</span>
-              He/Him
-            </p>
+            {details.companyIconUrl && (
+              <img
+                src={details.companyIconUrl}
+                alt={`${details.companyIconUrl} logo`}
+                className="max-h-6"
+              />
+            )}
+            {details.companyName}
+            {details.pronouns && (
+              <p className="flex items-center gap-2 text-gray-500">
+                <span className="font-black text-gray-500 pb-2">.</span>
+                {details.pronouns}
+              </p>
+            )}
           </li>
         </ul>
         <footer className="flex items-center text-gray-500 pb-8">
-          <span aria-label="Canadian flag" role="img" className="text-xl">
+          <span
+            aria-label={`Flag of ${location.country}`}
+            role="img"
+            className="text-xl"
+          >
             {flagEmoji}
           </span>
-          <span className="text-lg pl-4">Vancouver, Canada</span>
+          <span className="text-lg pl-4">
+            {location.city}, {location.country}
+          </span>
         </footer>
       </section>
     </article>
