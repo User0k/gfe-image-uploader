71a1a6b1bde41733c085a60c3b07d6e18f817205
user0k <glucus2@yandex.ru>
2025-05-24 13:48:45 +0300

    feat: dnd css styles, basic dnd handlers

--- src/components/UploadModal/DropZone.tsx
+++ src/components/UploadModal/DropZone.tsx
@@ -1,9 +1,40 @@
+import { useState, type DragEventHandler } from "react";
+import { twMerge } from "tailwind-merge";
 import UploadIcon from "../../icons/upload.svg?react";
 
 export function DropZone() {
-  const handleClick = () => {
-    console.log("upload");
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleDragEnter: DragEventHandler<HTMLLabelElement> = (e) => {
+    e.preventDefault();
+    setIsDragging(true);
   };
+
+  const handleDragOver: DragEventHandler<HTMLLabelElement> = (e) => {
+    e.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave: DragEventHandler<HTMLLabelElement> = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+  };
+
+  const handleDrop: DragEventHandler<HTMLLabelElement> = (e) => {
+    e.preventDefault();
+    setIsDragging(false);
+    console.log(e.dataTransfer.files);
+  };
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files ?? [];
+    console.log(files);
+    event.target.value = "";
+  };
+
+  const bgStyles = isDragging
+    ? "bg-stone-100"
+    : "bg-stone-50 hover:bg-stone-100";
 
   return (
     <>
@@ -14,12 +45,19 @@ export function DropZone() {
         hidden
         accept="image/png, image/jpeg"
         id="modal-upload"
+        onChange={handleFileChange}
       />
       <label
         htmlFor="modal-upload"
         role="button"
-        onClick={handleClick}
-        className="rounded-lg flex flex-col items-center mt-6 border border-gray-200 md:px-32 sm:px-16 px-12 py-12 cursor-grab"
+        onDragEnter={handleDragEnter}
+        onDragOver={handleDragOver}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+        className={twMerge(
+          "rounded-lg flex flex-col items-center mt-6 border border-gray-200 md:px-32 sm:px-16 px-12 py-12 cursor-grab",
+          bgStyles,
+        )}
       >
         <div className="rounded-full bg-white p-3 shadow-md">
           <UploadIcon className="w-6 h-6 fill-indigo-700" />
